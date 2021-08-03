7cf8ada5814fa96595006eaaeb9bb1a8e6d58aed
Mubashira Qari <81127634+uzmabb182@users.noreply.github.com>
2021-08-03 19:06:06 -0400

    size of the marker included

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,20 @@
-// A function to determine the marker size based on the population
-function markerSize(population) {
-  return Math.sqrt(population) * 50;
-}
+//=====================================================================================================================
+// Level 1: Import & Visualize the Data
+// "All Earthquakes from the Past 7 Days", you will be given a JSON representation of that data.
+//=====================================================================================================================
+
+// Create a map using Leaflet that plots all of the earthquakes from your data set based on their longitude and latitude.
+
+// Your data markers should reflect the magnitude of the earthquake by their size and and depth of the earthquake by color. Earthquakes with higher magnitudes should appear larger and earthquakes with greater depth should appear darker in color.
+
+// HINT: The depth of the earth can be found as the third coordinate for each earthquake.
+
+// Include popups that provide additional information about the earthquake when a marker is clicked.
+
+// Create a legend that will provide context for your map data.
+
+//=====================================================================================================================
+
 
 // Use this link to get the GeoJSON data.
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
@@ -11,35 +24,48 @@ var cityMarkers = [];
 
 d3.json(link).then(function (data) {
   console.log(data)
-  var geojsonFeature = data.features
-  console.log(geojsonFeature)
+  console.log(data.features)
 
   // Define a function that we want to run once for each feature in the features array.
   // Give each feature a popup that describes the magnitude and place of the earthquake.
-  // 'onEachFeature' function is used inside the 'createFeatures' fuction
+
+
   function onEachFeature(feature, layer) {
-    //console.log(feature)
+    console.log(feature)
+    //console.log(feature.properties.mag)
+    console.log(feature.geometry.coordinates[2])
     layer.bindPopup(`<h3>Magnitude: ${feature.properties.mag}</h3><hr><p>Place: ${(feature.properties.place)}</p>`);
   }
 
+  // A function to generate color based on depth
   function generateColor(depth) {
-    if (depth > 100) {
+    if (depth > 10) {
       return "#ea2c2c"
     }
-    if (depth > 75) {
+    if (depth > .75) {
       return "#ea822c"
     }
-    if (depth > 50) {
+    if (depth > .50) {
       return "#fbff00"    
     }
-    if (depth > 25) {
+    if (depth > .25) {
       return "#72fa41"
     }
+    else {
+      return "#800080"
+    }
   }
-  
+
+
+  // A function to determine the marker size based on the magnitude
+function markerSize(magnitude) {
+  return Math.sqrt(magnitude) * 10;
+}
+
+ // Assigning the style by calling function based on requirement
   function generateStyle(feature) { 
     return {
-      radius: 10,
+      radius: markerSize(feature.properties.mag),
       color: generateColor(feature.geometry.coordinates[2]),
       fillColor: generateColor(feature.geometry.coordinates[2]),
       opacity: 1
@@ -66,9 +92,8 @@ d3.json(link).then(function (data) {
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   });
 
-  // Create two separate layer groups: one for the city markers and another for the state markers.
+  // Create a separate layer group for magnitude
 
-  // var cities = L.layerGroup(earthquakes);
 
   // Create a baseMaps object.
   var baseMaps = {
@@ -96,3 +121,19 @@ d3.json(link).then(function (data) {
   }).addTo(myMap);
 
 })
+
+
+//===================================================================================================================
+// Level 2: The USGS wants you to plot a second data set on your map to illustrate the relationship 
+//between tectonic plates and seismic activity. You will need to pull in a second data set and visualize it 
+//alongside your original set of data. Data on tectonic plates can be found at https://github.com/fraxen/tectonicplates.
+//=====================================================================================================================
+
+// In this step, you will:
+
+// Plot a second data set on our map.
+
+// Add a number of base maps to choose from as well as separate out our two different data sets into overlays that can be turned on and off independently.
+
+// Add layer controls to our map.
+//===================================================================================================
